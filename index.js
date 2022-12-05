0cd36f448995d34819dec562e5b23a08c2c3d3e4
mitchelleokemwa <mitchelleokemwa@gmail.com>
2022-12-05 10:18:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+  function saturdayFun(activity = 'roller-skate'){
+    return(`This Saturday, I want to ${activity}!`);
+  }
+
+  saturdayFun('roller-skate');
+
+  function mondayWork(activity = 'go to the office'){
+    return(`This Monday, I will ${activity}.`);
+  }
+
+  mondayWork('go to the office');
+
+  function wrapAdjective(wrapper = "*"){
+    function desplay(compliment = 'special'){ 
+    return(`You are ${wrapper}${compliment}${wrapper}!`);
+  };
+  return desplay;
+  }
+  wrapAdjective("||");
+  
+
